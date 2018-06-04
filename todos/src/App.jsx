c9f7a9ff21249a267fd74fb801f0ab4cd285b749
Sam <swkane@indiana.edu>
2018-06-04 10:57:55 -0400

    fixed all warnings

--- todos/src/App.jsx
+++ todos/src/App.jsx
@@ -7,11 +7,15 @@ class TodoItem extends Component {
     completed: this.props.completed
   }
 
+  onChange = () => {
+
+  }
+
   render() {
     return (
-      <li className={this.state.completed}>
+      <li className={this.state.completed ? 'completed' : ''}>
         <div className="view">
-          <input className="toggle" type="checkbox" checked={this.state.completed} />
+          <input onChange={this.onChange} className="toggle" type="checkbox" checked={this.state.completed} />
             <label>{this.props.text}</label>
             <button className="destroy"></button>
 				</div>
@@ -21,38 +25,45 @@ class TodoItem extends Component {
 }
 
 class TodoList extends Component {
-  state = {
-    todos: todoList
-  }
 
   render() {
     return (
-      <ul class="todo-list">
-        {this.state.todos.map(todo => <TodoItem key={todo.id} text={todo.title} completed={todo.completed} />)}  
+      <ul className="todo-list">
+        {this.props.todos.map(todo => <TodoItem key={todo.id} text={todo.title} completed={todo.completed} />)}  
 			</ul>
     );
   }
 }
 
 class App extends Component {
+  state = {
+    todos: todoList
+  }
+
+  addTodo = (target) => {
+    this.setState( prevState => {
+      return { todos: [ ...prevState.todos, target.value ] };
+    });
+  }
+
   render() {
     return (
-      <section class="todoapp">
-        <header class="header">
+      <section className="todoapp">
+        <header className="header">
           <h1>todos</h1>
-          <input class="new-todo" placeholder="What needs to be done?" autofocus />
+          <input onSubmit={this.addTodo} className="new-todo" placeholder="What needs to be done?" autoFocus />
 			  </header>
           {/* <!-- This section should be hidden by default and shown when there are todos --> */}
-			  <section class="main">
-          <TodoList />
+			  <section className="main">
+          <TodoList todos={this.state.todos} />
 			  </section>
         {/* <!-- This footer should hidden by default and shown when there are todos --> */}
-			  <footer class="footer">
+			  <footer className="footer">
           {/* <!-- This should be `0 items left` by default --> */}
-				  <span class="todo-count"><strong>0</strong> item(s) left</span>
+				  <span className="todo-count"><strong>0</strong> item(s) left</span>
           {/* <!-- Remove this if you don't implement routing --> */}
 				  {/* <!-- Hidden if no completed items are left ↓ --> */}
-				  <button class="clear-completed">Clear completed</button>
+				  <button className="clear-completed">Clear completed</button>
         </footer>
 		</section>
     );
