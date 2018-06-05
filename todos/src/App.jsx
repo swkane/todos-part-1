b29a623300bfcadfdcda1d60336387d7ecce62b0
Sam <swkane@indiana.edu>
2018-06-05 15:41:11 -0400

    implemented todo day 2

--- todos/src/App.jsx
+++ todos/src/App.jsx
@@ -3,21 +3,14 @@ import todoList from './todos.json';
 import './App.css';
 
 class TodoItem extends Component {
-  state = {
-    completed: this.props.completed
-  }
-
-  onChange = () => {
-
-  }
 
   render() {
     return (
-      <li className={this.state.completed ? 'completed' : ''}>
+      <li className={this.props.completed ? 'completed' : ''}>
         <div className="view">
-          <input onChange={this.onChange} className="toggle" type="checkbox" checked={this.state.completed} />
+          <input onChange={this.props.onCompleted} className="toggle" type="checkbox" checked={this.props.completed} />
             <label>{this.props.text}</label>
-            <button className="destroy"></button>
+            <button onClick={this.props.onDelete} className="destroy"></button>
 				</div>
 			</li>
     );
@@ -29,7 +22,7 @@ class TodoList extends Component {
   render() {
     return (
       <ul className="todo-list">
-        {this.props.todos.map(todo => <TodoItem key={todo.id} text={todo.title} completed={todo.completed} />)}  
+        {this.props.todos.map( (todo, i) => <TodoItem key={i} text={todo.title} completed={todo.completed} onDelete={this.props.onDelete(i)} onCompleted={this.props.onCompleted(i)} />)}  
 			</ul>
     );
   }
@@ -37,33 +30,93 @@ class TodoList extends Component {
 
 class App extends Component {
   state = {
-    todos: todoList
+    todos: todoList,
+    text: ''
   }
 
-  addTodo = (target) => {
-    this.setState( prevState => {
-      return { todos: [ ...prevState.todos, target.value ] };
-    });
+  // Data Structure of a New Todo (can also view ./todos.json)
+  // 
+  // newTodo: {
+  //   "userId": 1,
+  //   "id": 0,
+  //   "title": "",
+  //   "completed": false
+  // },
+
+  // adding a todo
+  handleEnter = (e) => {
+    if (e.key === "Enter") {
+      let newTodo = {
+        "userId": 1,
+        "id": 0,
+        "title": this.state.text,
+        "completed": false
+      };
+
+      this.setState(prevState => ({
+        todos: [...prevState.todos, newTodo],
+        text: ''
+      }));
+    } 
   }
+
+  // controlled adding todo input
+  handleChange = event => {
+    this.setState({text: event.target.value});
+  }
+
+  // checking a todo as complete
+  handleComplete = index => () => this.setState({
+    todos: this.state.todos.map((todo, i) => index === i ? {
+      ...todo,
+      completed: !todo.completed
+    } : todo)
+  });
+
+  // deleting a todo
+  handleDelete = index => () => this.setState({
+    todos: this.state.todos.filter((todo, i) => index !== i)
+  })
+
+  // deleting all todos marked as complete
+  handleDeleteAll = () => this.setState({
+    todos: this.state.todos.filter(todo => !todo.completed)
+  })
 
   render() {
     return (
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
-          <input onSubmit={this.addTodo} className="new-todo" placeholder="What needs to be done?" autoFocus />
+          <input onChange={this.handleChange}  onKeyUp={this.handleEnter} className="new-todo" value={this.state.text} autoFocus />
 			  </header>
           {/* <!-- This section should be hidden by default and shown when there are todos --> */}
 			  <section className="main">
-          <TodoList todos={this.state.todos} />
+          <TodoList todos={this.state.todos} onDelete={this.handleDelete} onCompleted={this.handleComplete} />
 			  </section>
         {/* <!-- This footer should hidden by default and shown when there are todos --> */}
 			  <footer className="footer">
           {/* <!-- This should be `0 items left` by default --> */}
 				  <span className="todo-count"><strong>0</strong> item(s) left</span>
-          {/* <!-- Remove this if you don't implement routing --> */}
+          <ul className="filters">
+            <li>
+              <a href="#/">
+                All
+							</a>
+            </li>
+            <li>
+              <a href="#/active">
+                Active
+							</a>
+            </li>
+            <li>
+              <a href="#/completed">
+                Completed
+							</a>
+            </li>
+          </ul>
 				  {/* <!-- Hidden if no completed items are left ↓ --> */}
-				  <button className="clear-completed">Clear completed</button>
+				  <button onClick={this.handleDeleteAll} className="clear-completed">Clear completed</button>
         </footer>
 		</section>
     );
